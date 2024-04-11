54777f3459d89cbbe91cecc914efe835fb6daf04
atheer <amawaldi984@rku.ac.in>
2024-04-12 04:36:43 +0530

    first commit

---
+++ firebase.config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBzJBaTzsfawZQmANjT-LPzUu9LU0tOJ74",
+    authDomain: "angularfirebase-d8153.firebaseapp.com",
+    databaseURL: "https://angularfirebase-d8153-default-rtdb.firebaseio.com",
+    projectId: "angularfirebase-d8153",
+    storageBucket: "angularfirebase-d8153.appspot.com",
+    messagingSenderId: "498612702449",
+    appId: "1:498612702449:web:526d149486a2e7b847ed50"
+  };
